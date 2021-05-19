51d763c4c6ca95d6c37400ad7220643739665a49
Documenter.jl <documenter@juliadocs.github.io>
2021-05-19 17:34:49 +0000

    build based on 00491f9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NetworkLayout","category":"page"},{"location":"#NetworkLayout","page":"Home","title":"NetworkLayout","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This is the Documentation for NetworkLayout.","category":"page"}]
+}
